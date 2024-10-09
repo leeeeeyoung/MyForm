68b53445a2c2e6557d7138dd568c687c952b84f8
wady1129 <134192971+wady1129@users.noreply.github.com>
2024-10-09 21:34:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -133,16 +133,6 @@ const questions = [
     }
 ];
 
-const demo = [
-    {
-        question: "0.如果您想左轉，並且看到顯示的交通號誌，您會...",
-        options: ["直接左轉", "讓行，等待間隙", "停下"],
-        answer: 2,
-        video: "video0.mp4"
-    },
-];
-
-
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -165,18 +155,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const basicInfoForm = document.getElementById("basic-info-form");
     const mainMenu = document.getElementById("main-menu");
     const quizContainer = document.getElementById("quiz-container");
-    const demoContainer = document.getElementById("demo-container");
     const submitInfoButton = document.getElementById("submit-info-button");
     const startButton = document.getElementById("start-button");
-    const demoNextButton = document.getElementById("demo-next-button");
     const questionElement = document.getElementById("question");
     const optionsContainer = document.getElementById("options-container");
     const videoElement = document.getElementById("video");
     const agreeButton = document.getElementById("agree-button");
     const surveyIntro = document.getElementById("survey-intro");
-    const demoVideoElement = document.getElementById("demo-video");
-    const demoOptionsContainer = document.getElementById("demo-options-container");
-    const demoFeedback = document.getElementById("demo-feedback");
+
+    basicInfoForm.classList.add("hidden");
     
     // 當按下同意按鈕後，隱藏問卷說明並顯示基本資料表單
     agreeButton.addEventListener("click", () => {
@@ -212,90 +199,16 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     };
 
-    startButton.addEventListener("click", () => {
-        mainMenu.classList.add("hidden");
-        demoContainer.classList.remove("hidden");
-    });
-    
-
-
-    function loadDemoQuestion() {
-        const currentQuestion = demo[0];
-        let firstplay = false;
-
-        demoOptionsContainer.innerHTML = "";
-        demoOptionsContainer.classList.add("hidden");
-        demoFeedback.classList.add("hidden");
-
-        demoVideoElement.src = currentQuestion.video;
-        // 移除播放控制
-        demoVideoElement.removeAttribute("controls"); // 確保沒有顯示控制列
-        // 自動播放影片並禁止手動控制
-        demoVideoElement.setAttribute("autoplay", "true");
-        demoVideoElement.setAttribute("muted", "true");
-        demoVideoElement.setAttribute("playsinline", "true");
-        demoVideoElement.setAttribute("webkit-playsinline", "true");
-        demoVideoElement.controls = false; // 確保播放控制被禁用
-
-        // 禁止用戶使用鍵盤控制影片
-        demoVideoElement.addEventListener("keydown", (event) => {
-            event.preventDefault();
-        });
-
-        demoVideoElement.onplaying = () => {
-            if (!firstplay) {
-                startTime = new Date();
-                firstplay = true;
-
-                const intervalId = setInterval(() => {
-                    if (demoVideoElement.currentTime >= 3) {
-                        clearInterval(intervalId);
-                        displayDemoOptions(currentQuestion);
-                    }
-                }, 100);
-            }
-        };
-
-        demoVideoElement.play().catch((error) => {
-            console.error("影片無法自動播放: ", error);
-        });
-    }
-
-    function displayDemoOptions(currentQuestion) {
-        currentQuestion.options.forEach((option, i) => {
-            const button = document.createElement("button");
-            button.textContent = option;
-            button.onclick = () => selectDemoOption(i);
-            demoOptionsContainer.appendChild(button);
-        });
-        demoOptionsContainer.classList.remove("hidden");
-    }
-
-    function selectDemoOption(selectedIndex) {
-        const endTime = new Date();
-        const timeTaken = ((endTime - startTime) / 1000 - 3.0).toFixed(2);
-
-        const correct = selectedIndex === demo[0].answer;
-        if (correct) {
-            demoFeedback.textContent = `正確答案！您回答正確！反應時間：${timeTaken} 秒`;
-        } else {
-            demoFeedback.textContent = `錯誤答案，請再檢視示範影片。反應時間：${timeTaken} 秒`;
-        }
-        demoFeedback.classList.remove("hidden");
-        demoOptionsContainer.classList.add("hidden");
-    }
-
-
-    demoNextButton.onclick = () => {
+    startButton.onclick = () => {
         localStorage.removeItem("quizResults");
         answers = [];
         currentQuestionIndex = 0;
         shuffle(questions); // 打亂問題順序並更新題號
-        demoContainer.classList.add("hidden");
+        mainMenu.classList.add("hidden");
         quizContainer.classList.remove("hidden");
         loadQuestion(currentQuestionIndex);
     };
-    
+
     function loadQuestion(index) {
         const currentQuestion = questions[index];
         let hasOptionsShown = false;
