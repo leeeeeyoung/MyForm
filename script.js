cfa072d01dde8a0c00fba3c9525f2c329266d711
wady1129 <134192971+wady1129@users.noreply.github.com>
2024-10-14 16:45:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -223,7 +223,6 @@ document.addEventListener("DOMContentLoaded", () => {
     function loadQuestionContent(questionData, isDemoQuestion) {
         let hasOptionsShown = false;
         let firstplay = false;
-        let firstFullPlayDone = false;
     
         questionElement.textContent = questionData.question;
         optionsContainer.innerHTML = "";
@@ -245,24 +244,29 @@ document.addEventListener("DOMContentLoaded", () => {
             if (!firstplay) {
                 startTime = new Date();
                 firstplay = true;
+    
+                const intervalId = setInterval(() => {
+                    if (videoElement.currentTime >= 3) {
+                        clearInterval(intervalId);
+                        if (!hasOptionsShown) {
+                            displayOptions(questionData, isDemoQuestion);
+                            hasOptionsShown = true;
+                        }
+                    }
+                }, 100);
             }
         };
     
         videoElement.onended = () => {
-            if (!firstFullPlayDone) {
-                // 第一次完整播放結束
-                firstFullPlayDone = true;
-                videoElement.currentTime = 3;
-                videoElement.loop = true;
-                videoElement.play().catch((error) => {
-                    console.error("影片無法自動播放: ", error);
-                });
-            } else {
-                if (!hasOptionsShown) {
-                    displayOptions(questionData, isDemoQuestion);
-                    hasOptionsShown = true;
-                }
+            if (!hasOptionsShown) {
+                displayOptions(questionData, isDemoQuestion);
             }
+            // 影片第一次播放完畢後，回到第3秒並進行循環播放
+            videoElement.currentTime = 3;
+            videoElement.loop = true;
+            videoElement.play().catch((error) => {
+                console.error("影片無法自動播放: ", error);
+            });
         };
     
         videoElement.play().catch((error) => {
